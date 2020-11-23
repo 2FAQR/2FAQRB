4591f43d2b585582b02668b0ab876200c21f8cec
Rohit Jain <rohitjain18005@gmail.com>
2020-11-24 01:44:11 +0530

    Update config.py

--- app/main/config.py
+++ app/main/config.py
@@ -7,7 +7,7 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 
 
 class Config:
-    SECRET_KEY = os.getenv('SECRET_KEY', 'my_precious_secret_key')
+    SECRET_KEY = os.getenv('SECRET_KEY1', 'my_precious_secret_key')
     DEBUG = False
 
 
